861e3a27683b34d69fde9363cba1fe2377c4255c
fatcatt <zhaotianqi01@qiyi.com>
2023-06-15 15:34:19 +0800

    feat: 移动端 release

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -8,17 +8,34 @@ import './index.css';
 
 function FooterCom() {
     return (
-        <div style={{height: '80px', backgroundColor: '#000', paddingTop: '40px'}}>
+        <div style={{height: '80px', backgroundColor: '#000', paddingTop: '40px', paddingLeft: '64px', paddingRight: '64px'}}>
             {/* <span>
                 <img src={require('../../statics/logo.jpeg')} style={{borderRadius: '20px', width: '40px'}} />
             </span> */}
+            <span style={{color: '#c3c3c3'}}>京ICP备2023013566号-1</span>
             <Space style={{float: 'right'}}>
                 <Tooltip title="使用Command+D可将网页添加到书签">
                     <div className="iconBox">
                         <Star theme="outline" size="22" fill="#eee" />
                     </div>
                 </Tooltip>
-                <Tooltip title="扫描二维码，关注茅山易学术数公众号">
+                <Tooltip
+                    style={{width: '400px'}}
+                    title={() => {
+                        return (
+                            <Space style={{padding: '10px'}}>
+                                <div style={{marginRight: '8px'}}>
+                                    <div>八字咨询</div>
+                                    <img src={require('../../statics/shulan.jpg')} style={{width: '100px'}}></img>
+                                </div>
+                                <div>
+                                    <div>风水咨询</div>
+                                    <img src={require('../../statics/gaocan.jpg')} style={{width: '100px'}}></img>
+                                </div>
+                            </Space>
+                        );
+                    }}
+                >
                     <div className="iconBox">
                         <Wechat theme="outline" size="22" fill="#eee" />
                     </div>

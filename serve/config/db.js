bb8a95abec282c31d3902f67b028bf1047a466b6
fatcatt <zhaotianqi01@qiyi.com>
2023-06-23 15:37:03 +0800

    feat: 服务端数据库

--- serve/config/db.js
+++ serve/config/db.js
@@ -1,13 +1,20 @@
 var mysql = require('mysql');
-const config = {
+const devConfig = {
     host: 'localhost',
     port: 3306, //端口号
     database: 'users',
     user: 'root',
     password: '12345678'
 };
-
-var pool = mysql.createPool(config);
+const prodConfig = {
+    host: '124.221.158.62',
+    port: 3306, //端口号
+    database: 'users',
+    user: 'root',
+    password: 'Gaocanyixue123.'
+};
+console.log(process.env.NODE_ENV);
+var pool = mysql.createPool(process.env.NODE_ENV === 'production' ? prodConfig : devConfig);
 // console.log(pool);
 // pool.getConnection(function (err, connection) {
 //     if (err) {
